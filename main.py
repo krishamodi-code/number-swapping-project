326b1d4e23167afbff8ccecca96c0413c58d0b2a
krishamodi-code <krisha.modi24@gmail.com>
2024-10-27 16:56:07 +0530

    iu

---
+++ main.py
@@ -0,0 +1,18 @@
+x= 9
+y= 8
+z= 7
+
+print("Before Swapping:")
+print("x =", x)
+print("y =", y)
+print("z =", z)
+
+temp1= z
+z= y
+y= x
+x= temp1
+
+print("\nAfter Swapping:")
+print("x =", x)
+print("y =", y)
+print("z =", z)
